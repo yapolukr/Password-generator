dc3b39cf0be9fbf0e51233d1a89ef4d5487838ac
Yana Frolova <frolova.y147@gmail.com>
2022-07-19 18:43:41 +0200

    Basic structure password.html

--- generator/urls.py
+++ generator/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home),
-    path('password', views.password, name='password')
+    path('', views.home, name='home'),
+    path('password', views.password, name='password'),
+    path('about', views.about, name='about')
 ]
--- generator/views.py
+++ generator/views.py
@@ -8,17 +8,28 @@ def password(request):
 
     characters = list('qwertyuiopasdfghjklzxcvbnm')
 
-    length = 10
+    if request.GET.get('uppercase'):
+        characters.extend(list("QWERTYUIOPASDFGHJKLZXCVBNM"))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+*<>?'))
+
+
+    length = int(request.GET.get('length'))
 
     thepassword = ''
 
     for x in range(length):
         thepassword += random.choice(characters)
 
-    uppercase = list('QWERTYUIOPASDFGHJKLZXCVBNM')
-
-    numbers = list('1234567890')
-
-    characters = list('~!@#$%^&*()_+{}:"<>?-+')
-
     return render(request, 'generator/password.html', {'password':thepassword})
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def copy(request):
+    global thepassword
+    return (request, thepassword)
