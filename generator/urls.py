9caf1cc49b2274a50927008e47adc536e2e20d83
Yana Frolova <frolova.y147@gmail.com>
2022-07-24 15:14:15 +0200

    Added personal cabinet

--- generator/urls.py
+++ generator/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('password', views.password, name='password'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('sign_up', views.sign_up, name='sign_up'),
+    path('current', views.currenttodos, name='currentpasswords'),
+    path('logout', views.logoutuser, name='logoutuser'),
+    path('login', views.loginuser, name='loginuser'),
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import random
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
 # Create your views here.
 def home(request):
     return render(request, 'generator/home.html')
@@ -33,3 +37,41 @@ def about(request):
 def copy(request):
     global thepassword
     return (request, thepassword)
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def sign_up(request):
+    if request.method == 'GET':
+        return render(request, 'generator/sign_up.html', {'form':UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password= request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currentpasswords')
+            except IntegrityError:
+                return render(request, 'generator/sign_up.html', {'form': UserCreationForm(), 'error':'This name already exists'})
+
+        else:
+            return render(request, 'generator/sign_up.html', {'form': UserCreationForm(), 'error':'The passwords did not match'})
+
+def currenttodos(request):
+    return render(request, 'generator/currentpasswords.html')
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'generator/login.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'generator/login.html', {'form': AuthenticationForm(), 'error':'Username or password did not match'})
+        else:
+            login (request, user)
+            return redirect('currentpasswords')
